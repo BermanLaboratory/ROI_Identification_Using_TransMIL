c154a15c4018603e176aef7bd22650a5ff8d87df
BermanLab <pranavkataria978@gmail.com>
2022-08-24 06:37:49 +0000

    Setting Up repo

---
+++ src/models/architechture/TransMIL_interface.py
@@ -0,0 +1,258 @@
+import sys
+import numpy as np
+import inspect
+import importlib
+import random
+import pandas as pd
+
+#---->
+from models.architechture.MyOptimizer import create_optimizer
+from models.architechture.MyLoss import create_loss
+
+#---->
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics
+
+#---->
+import pytorch_lightning as pl
+
+#-----> val loss
+
+import torch
+import torch.nn.functional as F
+def cross_entropy_torch(x, y):
+    x_softmax = [F.softmax(x[i]) for i in range(len(x))]
+    x_log = torch.tensor([torch.log(x_softmax[i][y[i]]) for i in range(len(y))])
+    loss = - torch.sum(x_log) / len(y)
+    return loss
+
+class  ModelInterface(pl.LightningModule):
+
+    #---->init
+    def __init__(self, model, loss, optimizer, **kargs):
+        super(ModelInterface, self).__init__()
+        self.save_hyperparameters()
+        self.load_model()
+        self.loss = create_loss(loss)
+        self.optimizer = optimizer
+        self.n_classes = model.n_classes
+        self.log_path = kargs['log']
+      
+        #---->acc
+        self.data_train = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+        self.data_val = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+        self.data_test = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+        #---->Metrics
+        if self.n_classes > 2: 
+            self.AUROC = torchmetrics.AUROC(num_classes = self.n_classes, average = 'macro')
+            metrics = torchmetrics.MetricCollection([torchmetrics.Accuracy(num_classes = self.n_classes,
+                                                                           average='micro'),
+                                                     torchmetrics.CohenKappa(num_classes = self.n_classes),
+                                                     torchmetrics.F1Score(num_classes = self.n_classes,
+                                                                     average = 'macro'),
+                                                     torchmetrics.Recall(average = 'macro',
+                                                                         num_classes = self.n_classes),
+                                                     torchmetrics.Precision(average = 'macro',
+                                                                            num_classes = self.n_classes),
+                                                     torchmetrics.Specificity(average = 'macro',
+                                                                            num_classes = self.n_classes)])
+        else : 
+            self.AUROC = torchmetrics.AUROC(num_classes=2, average = 'macro')
+            metrics = torchmetrics.MetricCollection([torchmetrics.Accuracy(num_classes = 2,
+                                                                           average = 'micro'),
+                                                     torchmetrics.CohenKappa(num_classes = 2),
+                                                     torchmetrics.F1Score(num_classes = 2,
+                                                                     average = 'macro'),
+                                                     torchmetrics.Recall(average = 'macro',
+                                                                         num_classes = 2),
+                                                     torchmetrics.Precision(average = 'macro',
+                                                                            num_classes = 2)])
+        self.valid_metrics = metrics.clone(prefix = 'val_')
+        self.test_metrics = metrics.clone(prefix = 'test_')
+
+        #--->random
+        self.shuffle = kargs['data'].data_shuffle
+        self.count = 0
+
+
+   
+
+
+    #---->remove v_num
+    def get_progress_bar_dict(self):
+        # don't show the version number
+        items = super().get_progress_bar_dict()
+        items.pop("v_num", None)
+        return items
+
+    def training_step(self, batch, batch_idx):
+        #---->inference
+    
+        data, label= batch
+        results_dict = self.model(data=data, label=label)
+        logits = results_dict['logits']
+        Y_prob = results_dict['Y_prob']
+        Y_hat = results_dict['Y_hat']
+        
+
+        #---->loss
+        loss = self.loss(logits, label)
+
+        #---->acc log
+        Y_hat = int(Y_hat)
+        Y = int(label)
+        self.data_train[Y]["count"] += 1
+        self.data_train[Y]["correct"] += (Y_hat == Y)
+
+        return {'loss': loss} 
+
+    def training_epoch_end(self, training_step_outputs):
+        for c in range(self.n_classes):
+            count = self.data_train[c]["count"]
+            correct = self.data_train[c]["correct"]
+            if count == 0: 
+                acc = None
+              
+            else:
+                acc = float(correct) / count
+                
+            print('class {}: acc {}, correct {}/{}'.format(c, acc, correct, count))
+        self.data_train= [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+
+    def validation_step(self, batch, batch_idx):
+        data, label = batch
+        results_dict = self.model(data=data, label=label)
+        logits = results_dict['logits']
+        Y_prob = results_dict['Y_prob']
+        Y_hat = results_dict['Y_hat']
+
+
+        #---->acc log
+        Y = int(label)
+        self.data_val[Y]["count"] += 1
+        # print(self.data[Y]['count'])
+        self.data_val[Y]["correct"] += (Y_hat.item() == Y)
+
+        return {'logits' : logits, 'Y_prob' : Y_prob, 'Y_hat' : Y_hat, 'label' : label}
+
+
+    def validation_epoch_end(self, val_step_outputs):
+        logits = torch.cat([x['logits'] for x in val_step_outputs], dim = 0)
+        probs = torch.cat([x['Y_prob'] for x in val_step_outputs], dim = 0)
+        max_probs = torch.stack([x['Y_hat'] for x in val_step_outputs])
+        target = torch.stack([x['label'] for x in val_step_outputs], dim = 0)
+        
+        #---->
+        self.log('val_loss', cross_entropy_torch(logits, target), prog_bar=True, on_epoch=True, logger=True)
+        self.log('auc', self.AUROC(probs, target.squeeze()), prog_bar=True, on_epoch=True, logger=True)
+        self.log_dict(self.valid_metrics(max_probs.squeeze() , target.squeeze()),
+                          on_epoch = True, logger = True)
+
+        #---->acc log
+        for c in range(self.n_classes):
+            count = self.data_val[c]["count"]
+            correct = self.data_val[c]["correct"]
+            if count == 0: 
+                acc = None
+            else:
+                acc = float(correct) / count
+            print('class {}: acc {}, correct {}/{}'.format(c, acc, correct, count))
+        self.data_val = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+        
+        #---->random, if shuffle data, change seed
+        if self.shuffle == True:
+            self.count = self.count+1
+            random.seed(self.count*50)
+    
+
+
+    def configure_optimizers(self):
+        optimizer = create_optimizer(self.optimizer, self.model)
+        return [optimizer]
+
+    def test_step(self, batch, batch_idx):
+        data, label = batch
+        results_dict = self.model(data=data, label=label)
+        logits = results_dict['logits']
+        Y_prob = results_dict['Y_prob']
+        Y_hat = results_dict['Y_hat']
+        attentions_1 = results_dict['attentions_1']
+        attentions_2 = results_dict['attentions_2']
+        add_length = results_dict['add_length']
+
+        file_name = '632'
+        file_name = str(add_length) + 'kimianet500'+ '.npy'
+        # print(file_name)
+        # print(type(file_name[0]))
+
+        # np.save(file_name, attentions_1.cpu().detach().numpy())
+        # np.save('data.npy',attentions_2)
+        np.save(file_name,attentions_2.cpu().detach().numpy())
+        #---->acc log
+        Y = int(label)
+        self.data_test[Y]["count"] += 1
+        self.data_test[Y]["correct"] += (Y_hat.item() == Y)
+
+        return {'logits' : logits, 'Y_prob' : Y_prob, 'Y_hat' : Y_hat, 'label' : label}
+
+    # def test_epoch_end(self, output_results):
+    #     probs = torch.cat([x['Y_prob'] for x in output_results], dim = 0)
+    #     max_probs = torch.stack([x['Y_hat'] for x in output_results])
+    #     target = torch.stack([x['label'] for x in output_results], dim = 0)
+        
+    #     #---->
+    #     auc = self.AUROC(probs, target.squeeze())
+    #     metrics = self.test_metrics(max_probs.squeeze() , target.squeeze())
+    #     metrics['auc'] = auc
+    #     for keys, values in metrics.items():
+    #         print(f'{keys} = {values}')
+    #         metrics[keys] = values.cpu().numpy()
+    #     print()
+    #     #---->acc log
+    #     for c in range(self.n_classes):
+    #         count = self.data_test[c]["count"]
+    #         correct = self.data_test[c]["correct"]
+    #         if count == 0: 
+    #             acc = None
+    #         else:
+    #             acc = float(correct) / count
+    #         print('class {}: acc {}, correct {}/{}'.format(c, acc, correct, count))
+    #     self.data_test = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+    #     #---->
+    #     result = pd.DataFrame([metrics])
+    #     result.to_csv('result.csv')
+
+
+    def load_model(self):
+        name = self.hparams.model.name
+        # Change the `trans_unet.py` file name to `TransUnet` class name.
+        # Please always name your model file name as `trans_unet.py` and
+        # class name or funciton name corresponding `TransUnet`.
+        # print(name)
+        if '_' in name:
+            camel_name = ''.join([i.capitalize() for i in name.split('_')])
+        else:
+            camel_name = name
+        try:
+            Model = getattr(importlib.import_module(
+                f'models.architechture.{name}'), camel_name)
+        except:
+            raise ValueError('Invalid Module File Name or Invalid Class Name!')
+        self.model = self.instancialize(Model)
+        pass
+
+    def instancialize(self, Model, **other_args):
+        """ Instancialize a model using the corresponding parameters
+            from self.hparams dictionary. You can also input any args
+            to overwrite the corresponding value in self.hparams.
+        """
+        class_args = inspect.getargspec(Model.__init__).args[1:]
+        inkeys = self.hparams.model.keys()
+        args1 = {}
+        for arg in class_args:
+            if arg in inkeys:
+                args1[arg] = getattr(self.hparams.model, arg)
+        args1.update(other_args)
+        return Model(**args1)
---
+++ src/models/train_transMIL.py
@@ -0,0 +1,176 @@
+import argparse
+from pathlib import Path
+import numpy as np
+import glob
+
+from data.features_interface import FeaturesInterface
+from models.architechture.TransMIL_interface import ModelInterface
+from utils.utils import *
+
+# pytorch_lightning
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning import loggers as pl_loggers
+
+
+# ----> load Callback
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+import os
+
+def load_callbacks(cfg):
+
+    Mycallbacks = []
+    # Make output path
+    output_path = cfg.log_path
+    output_path.mkdir(exist_ok=True, parents=True)
+
+    early_stop_callback = EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.00,
+        patience=cfg.General.patience,
+        verbose=True,
+        mode='min'
+    )
+    Mycallbacks.append(early_stop_callback)
+
+    if cfg.General.server == 'train' :
+        Mycallbacks.append(ModelCheckpoint(monitor = 'val_loss',
+                                         dirpath = str(cfg.log_path),
+                                         filename = '{epoch:02d}-{val_loss:.4f}',
+                                         verbose = True,
+                                         save_last = True,
+                                         save_top_k = 1,
+                                         mode = 'min',
+                                         save_weights_only = True))
+    return Mycallbacks
+
+from data.features import *
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import yaml
+from addict import Dict
+def read_yaml(fpath=None):
+    with open(fpath, mode="r") as file:
+        yml = yaml.load(file, Loader=yaml.Loader)
+        return Dict(yml)
+
+
+#--->Setting parameters
+def make_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stage', default='train', type=str)
+    parser.add_argument('--config', default='/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/config/TransMIL.yaml',type=str)
+    parser.add_argument('--gpus', default = [2])
+    parser.add_argument('--fold', default = 0)
+    args = parser.parse_args()
+    return args
+
+#---->main
+def main(cfg):
+
+    #---->Initialize seed
+    pl.seed_everything(cfg.General.seed)
+
+    #---->load loggers
+    # tb_logger = pl_loggers.TensorBoardLogger(save_dir="/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/logs")
+    wandb_logger = pl_loggers.WandbLogger(name='Final Dataset Kimianet 500',project='Multi_Instance_Learning')
+    # slide_data = per_slide_features('/mnt/largedrive0/katariap/feature_extraction/data/Dataset/kimianet_features/FineTuned_Model_Features_dict.pickle')
+    slide_data = per_slide_features('/mnt/largedrive0/katariap/feature_extraction/data/Dataset/kimianet_features/KimiaNet_Features_Final.pickle')
+    labels_dict = dataset_labels('/mnt/largedrive0/katariap/feature_extraction/data/Dataset/Data.csv')
+    dataset_features = '/mnt/largedrive0/katariap/feature_extraction/data/Dataset/kimianet_features/KimiaNet_Features_Final.pickle'
+    with open('/mnt/largedrive0/katariap/feature_extraction/data/Code/kimianet_feature_extractor/src/data/selected_tiles/selected_clustering_500_final.json', 'r') as f:
+        selected = json.load(f)
+    # slide_data_500 = per_slide_selected(dataset_features,selected)
+    validation_split = .1
+    test_split = 0.1
+    shuffle_dataset = True
+    random_seed= 13
+    # dataset = Features(slide_data,labels_dict)
+    slide_list = []
+    json_folder = '/mnt/largedrive0/katariap/feature_extraction/data/Dataset/DenseNet_Features/'
+    with os.scandir(json_folder) as files:
+        for file in files:
+            slide_list.append(file.path)
+        
+    print(len(slide_list))
+    dataset = FeatureJson(slide_list,labels_dict)
+    # dataset = Features(slide_data_500,labels_dict)
+    dataset_size = len(dataset)
+
+    print(slide_list[183])
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+
+    test_indices = [183]
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    sampler = {'train':train_sampler,'val':valid_sampler,'test':test_sampler}
+
+    #---->Define Data 
+    DataInterface_dict = {'slide_feature_data':slide_data,
+                'labels_dict': labels_dict,
+                'sampler': sampler,
+                }
+
+    dm = FeaturesInterface(**DataInterface_dict)
+    
+    #---->Define Model
+    ModelInterface_dict = {'model': cfg.Model,
+                            'loss': cfg.Loss,
+                            'optimizer': cfg.Optimizer,
+                            'data': cfg.Data,
+                            'log': cfg.log_path
+                            }
+    model = ModelInterface(**ModelInterface_dict)
+    
+    #---->Instantiate Trainer
+    trainer = Trainer(
+        num_sanity_val_steps=0, 
+        logger=wandb_logger,
+        callbacks=cfg.callbacks,
+        max_epochs= 10,
+        gpus=[0],  
+        precision=cfg.General.precision,  
+        check_val_every_n_epoch=2,
+    )
+
+    #---->train or test
+    if cfg.General.server == 'train':
+        # print('Starting Training')
+        trainer.fit(model = model, datamodule = dm)
+    else:
+        # model_paths = list(cfg.log_path.glob('*.ckpt'))
+        # model_paths = [str(model_path) for model_path in model_paths if 'epoch' in str(model_path)]
+        # for path in model_paths:
+        #     print(path)
+
+        new_model = model.load_from_checkpoint('/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/models/Multi_Instance_Learning/x3wwm3y5/checkpoints/epoch=9-step=1770.ckpt',cfg=cfg)
+        trainer.test(model=new_model, datamodule=dm)
+
+if __name__ == '__main__':
+
+    args = make_parse()
+    cfg = read_yaml('/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/config/TransMIL.yaml')
+
+    #---->update
+    cfg.config = args.config
+    cfg.General.gpus = args.gpus
+    cfg.General.server = 'train'
+    cfg.Data.fold = args.fold
+
+
+    # Use Yaml to create a config file
+
+    #---->main
+    main(cfg)
+ 
