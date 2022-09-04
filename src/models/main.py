cac038c899fb91844e2cbf3e6baa3fb7bf1b6b11
BermanLab <pranavkataria978@gmail.com>
2022-09-04 15:05:18 +0000

    attention weights extraction through command line added

--- src/models/main.py
+++ src/models/main.py
@@ -12,7 +12,6 @@ import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning import loggers as pl_loggers
 
-# ----> load Callback
 
 from utils.utils import *
 import os
@@ -29,10 +28,7 @@ from addict import Dict
 #--->Setting parameters
 def make_parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--stage', default='train', type=str)
-    parser.add_argument('--config', default='/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/config/TransMIL.yaml',type=str)
-    parser.add_argument('--gpus', default = [2])
-    parser.add_argument('--fold', default = 0)
+    parser.add_argument('config',help = 'Path to the Configuration File' )
     args = parser.parse_args()
     return args
 
@@ -45,57 +41,11 @@ def main(cfg):
     #---->load loggers
     
     wandb_logger = pl_loggers.WandbLogger(name=cfg.General.run_name,project=cfg.General.project_name)
-    labels_dict = dataset_labels(cfg.Data.label_dir)
     
- 
-
-    if (cfg.Data.data_type == 'pickle') :
-
-        #  print('Hello')
-         slide_data = per_slide_features(cfg.Data.data_file)
-         dataset = Features(slide_data,labels_dict)
-
-    
-    else:
-
-        json_folder = cfg.Data.data_folder
-        slide_list = []
-        with os.scandir(json_folder) as files:
-            for file in files:
-                slide_list.append(file.path)
-
-        dataset = FeatureJson(slide_list,labels_dict)
-       
-   
-    validation_split = .1
-    test_split = 0.1
-    shuffle_dataset = True
-    random_seed= 13
-    slide_list = []
-
-    dataset_size = len(dataset)
-    indices = list(range(dataset_size))
-    split = int(np.floor(validation_split * dataset_size))
-    if shuffle_dataset:
-        np.random.seed(random_seed)
-        np.random.shuffle(indices)
-    train_indices, val_indices = indices[split:], indices[:split]
-
-
-    test_indices = [183]
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-    test_sampler = SubsetRandomSampler(test_indices)
-
-    sampler = {'train':train_sampler,'val':valid_sampler,'test':test_sampler}
 
     #---->Data_Interface 
-    DataInterface_dict = {'slide_feature_data':slide_data,
-                'labels_dict': labels_dict,
-                'sampler': sampler,
-                }
 
-    dm = FeaturesInterface(**DataInterface_dict)
+    dm = FeaturesInterface(cfg)
     
     #---->Define Model
     ModelInterface_dict = {'model': cfg.Model,
@@ -131,13 +81,11 @@ def main(cfg):
 if __name__ == '__main__':
 
     args = make_parse()
-    cfg = read_yaml('/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/config/bermanlab.yaml')
+    # cfg = read_yaml('/mnt/largedrive0/katariap/feature_extraction/data/Code/multi_instance_learning/src/config/bermanlab.yaml')
+    cfg = read_yaml(args['config'])
 
     #---->update
     cfg.config = args.config
-    cfg.General.gpus = args.gpus
-    cfg.General.mode = 'train'
-    cfg.Data.fold = args.fold
 
     #---->main
     main(cfg)
